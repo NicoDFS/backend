92e69b60a824b04d65120bfa7b4cfbe6ab463925
NicoDFS <cryptocoindev@protonmail.com>
2025-10-07 09:14:40 -0500

    Update Camalot addys

--- src/services/swap/swapQuoteService.ts
+++ src/services/swap/swapQuoteService.ts
@@ -7,7 +7,7 @@ import { multiRPCProviderService } from '../multichain/rpcProviderService';
 const ROUTER_ADDRESSES: Record<number, string> = {
   3888: '0x183F288BF7EEBe1A3f318F4681dF4a70ef32B2f3',  // KalySwap Router
   56: '0x10ED43C718714eb63d5aA57B78B54704E256024E',    // PancakeSwap Router
-  42161: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', // Uniswap V2 Router on Arbitrum
+  42161: '0xc873fEcbd354f5A56E00E710B90EF4201db2448d', // Camelot V2 Router on Arbitrum
 };
 
 /**
